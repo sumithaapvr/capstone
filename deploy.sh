345c4369e7df6f8c3fe5bb20b86ac700fd0cb7da
SUMITHAA P V R <sumithaapvr.22cse@kongu.edu>
2025-03-20 15:26:28 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
     echo hi123
     sh 'chmod +x build.sh'
     sh './build.sh'
-    docker login -u sumithaapvr -p dckr_pat_R8b7f4Zt_2C2IhvXlt3KQfHaAa4
+    docker login -u sumithaapvr -p dckr_pat_5mSrlWhvl5iRd7YeNheximdIMDA
     docker tag test1 sumithaapvr/newrepo1
     docker push sumithaapvr/newrepo1
     
