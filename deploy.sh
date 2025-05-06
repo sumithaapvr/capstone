d360715fcbd269a2f2c789daa618383acdd6f754
SUMITHAA P V R <sumithaapvr.22cse@kongu.edu>
2025-05-06 23:23:59 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
     sh 'chmod +x build.sh'
     sh './build.sh'
     docker login -u sumithaapvr -p dckr_pat_5mSrlWhvl5iRd7YeNheximdIMDA
-    docker tag test1 sumithaapvr/newrepo1
-    docker push sumithaapvr/newrepo1
+    docker tag test1 sumithaapvr/devopsproject
+    docker push sumithaapvr/devopsproject
     docker-compose up -d
     
